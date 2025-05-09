079427f0123eb7b0a9141002063da3e72f82056f
AlonsoCampi09 <alocam01@ucm.es>
2025-05-09 11:48:47 +0200

    cinematica ya terminada
    falta conectarla con las demás escenas

--- CinematicaInicial.cs
+++ CinematicaInicial.cs
@@ -30,48 +30,105 @@ public partial class CinematicaInicial : Node2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
-		//Si pulsas 'Enter', saltas la escena. 
+        //Si pulsas 'Enter', saltas la escena.
+        if (Input.IsKeyLabelPressed(Key.Enter)) {
+            GD.Print("Saltar escena"); 
+        }
 	}
 
 	//Funcion que reproduce la escena 
-	public void Escena() {
+	public async void Escena() {
 		String mensaje = "Un grupo de cuatro heroes se encuentra en la entrada de un coliseo";
-		if (CustomSignals.activado) DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
 
-		//Introduccion de Alex
-		alex.Visible = true;
+        if (CustomSignals.activado)
+        {
+            CustomSignals.Instance.repetir = mensaje;
+            DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
+
+        }
+        while (DisplayServer.TtsIsSpeaking())
+        {
+            await ToSignal(GetTree().CreateTimer(0.1f), "timeout"); // Espera 100ms
+        }
+
+        //Introduccion de Alex
+        alex.Visible = true;
 		mensaje = "El primero de ellos es Alex, un joven con gorra y sudadera roja.";
 
-        if (CustomSignals.activado) DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
+        if (CustomSignals.activado)
+        {
+            CustomSignals.Instance.repetir = mensaje;
+            DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
+
+        }
+        while (DisplayServer.TtsIsSpeaking())
+        {
+            await ToSignal(GetTree().CreateTimer(0.1f), "timeout"); // Espera 100ms
+        }
 
         //Introduccion de Cassandra
         cassandra.Visible = true;
         mensaje = "Le sigue Cassandra, una maga de pelo negro y tunica del mismo color.";
 
-        if (CustomSignals.activado) DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
+        if (CustomSignals.activado)
+        {
+            CustomSignals.Instance.repetir = mensaje;
+            DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
+
+        }
+        while (DisplayServer.TtsIsSpeaking())
+        {
+            await ToSignal(GetTree().CreateTimer(0.1f), "timeout"); // Espera 100ms
+        }
+
 
         //Introduccion de Vyls
         vyls.Visible = true;
         mensaje = "Tambien se encuentra Vyls, un robot.....";
 
+        if (CustomSignals.activado)
+        {
+            CustomSignals.Instance.repetir = mensaje;
+            DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
 
-        if (CustomSignals.activado) DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
+        }
+        while (DisplayServer.TtsIsSpeaking())
+        {
+            await ToSignal(GetTree().CreateTimer(0.1f), "timeout"); // Espera 100ms
+        }
 
 
         //Introduccion de Ishi
         ishi.Visible = true;
         mensaje = "El ultimo miembro se trata de Ishimondo, una criatura humanoide azul con forma de gato, y una mascara que oculta su rostro.";
 
+        if (CustomSignals.activado)
+        {
+            CustomSignals.Instance.repetir = mensaje;
+            DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
 
-        if (CustomSignals.activado) DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
+        }
+        while (DisplayServer.TtsIsSpeaking())
+        {
+            await ToSignal(GetTree().CreateTimer(0.1f), "timeout"); // Espera 100ms
+        }
 
+        //Entrada a batalla 
+        mensaje = "Todos escuchan un ruido delante suya";
+        if (CustomSignals.activado)
+        {
+            CustomSignals.Instance.repetir = mensaje;
+            DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
 
-		//Entrada a batalla 
-		mensaje = "Todos escuchan un ruido delante suya";
-        if (CustomSignals.activado) DisplayServer.TtsSpeak(mensaje, CustomSignals.Instance.voiceId, CustomSignals.volumenTextToSpeach, 1, CustomSignals.velocidadTextToSpeach);
+        }
+        while (DisplayServer.TtsIsSpeaking())
+        {
+            await ToSignal(GetTree().CreateTimer(0.1f), "timeout"); // Espera 100ms
+        }
 
-		ruido.Play(); 
+        ruido.Play(); 
     }
+
 
 	private void On_ruido_puerta_finished() {
 		GD.Print("Entrando a la batalla"); 
